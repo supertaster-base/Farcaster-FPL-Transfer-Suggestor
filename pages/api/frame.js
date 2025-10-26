7a9211f0b08ca80a13bb7f386b939719e1f72b83
Lars Mislej <larsmislej@Larss-MacBook-Air.local>
2025-10-27 00:22:59 +0100

    Use sans-serif font for canvas renderer

--- pages/api/frame.js
+++ pages/api/frame.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
 
     // text
     ctx.fillStyle = "#ffffff";
-    ctx.font = "bold 64px Arial";
+    ctx.font = "bold 64px sans-serif";
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
     ctx.fillText("Hello Farcaster 👋", width / 2, height / 2);
