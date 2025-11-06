a5e933fcdc86f5f3f9c45b15319ea8d791ac60fe
Lars Mislej <larsmislej@Larss-MacBook-Air.local>
2025-11-06 13:19:10 +0100

    Fix globals.css import so Tailwind styles load

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,11 @@
+// pages/_app.js
+
+import "@/styles/globals.css";   // ✅ Load Tailwind + global styles
+
 export const config = {
-  runtime: 'nodejs',
+  runtime: "nodejs",
 };
 
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
-
